16ac96f50d194275d875457563f328eddfef376f
Bafou <antoine-59630@hotmail.fr>
2016-01-31 16:32:06 +0100

    Ajout d'un début de comportement pour User et Pwd et de parametre necessaire au fonctionnement de ftpRequest, ajout de la classe serveur

---
+++ ftp/src/Serveur.java
@@ -0,0 +1,52 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Serveur extends Thread {
+
+	private ServerSocket serveurSocket;
+	private Socket socket = new Socket();
+
+	/** Initialization of Serveur with a specific port **/
+	public void initialization(int port) {
+		try {
+			this.serveurSocket = new ServerSocket(port);
+			System.out.println("Initialization OK on: " + port);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Wait for a new connection from a Client
+	 * A new thread is created when a client is connected
+	 */
+	public void run() {
+		while (true) {
+			System.out.println("Waiting client ...");
+			try {
+				this.socket = this.serveurSocket.accept();
+				new Thread(new FtpRequest(this.socket,".")).start();
+			} catch (IOException e) {
+				System.out.println(e.getMessage());
+			}
+		}
+	}
+
+	public ServerSocket getServeurSocket() {
+		return serveurSocket;
+	}
+
+	public void setServeurSocket(ServerSocket serveurSocket) {
+		this.serveurSocket = serveurSocket;
+	}
+
+	public Socket getSocket() {
+		return socket;
+	}
+
+	public void setSocket(Socket socket) {
+		this.socket = socket;
+	}
+
+}
