28ca463a5d8eb2b71bed821a356b92c4a4128b05
Valtena <ethanvaltena@gmail.com>
2016-03-13 00:41:16 +0100

    connection buggué pas encore trouver pourquoi

--- rest/Source_java_8/src/main/java/car/tp2/FTPRessource.java
+++ rest/Source_java_8/src/main/java/car/tp2/FTPRessource.java
@@ -4,8 +4,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.SocketException;
 
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -16,16 +19,55 @@ import org.apache.commons.net.ftp.FTPClient;
 public class FTPRessource {
 	
 	private FTPClient ftp;
+	//use to reconnect after timeout connexion
+	private String user;
+	private String password;
 
 	public FTPRessource(){
 		ftp = new FTPClient();
 	}
+	
+	private void connexion(String user, String password) throws SocketException, IOException{
+		System.out.println("pseudo : "+ user);
+		System.out.println("password : "+ password);
+		ftp.connect("ftp.rpdiv.com", 21);
+		System.out.println("connect");
+		System.out.println(ftp.getReply());
+		System.out.println(ftp.user(user));
+		System.out.println("no erreur");
+		System.out.println(ftp.getReply());
+		System.out.println(ftp.pass(password));
+		System.out.println(ftp.getReply());
+	}
 
+	@GET
+	@Path("/auth")
+	@Produces("text/html")
+	public String getConnexionForm( @PathParam("var") String path ) {
+		return "<form method=\"POST\" action=\"auth\"><input name=\"login\" placeholder=\"login\" /><br /><input type=\"password\" name=\"psw\" placeholder=\"password\" /><br /><input type=\"submit\" value=\"Submit\"><br /></form>";
+	 }
+	
+	@POST
+	@Path("/auth")
+	@Produces("text/html")
+	public String postConnexionForm( @FormParam("login") String login, @FormParam("psw") String psw) {
+		this.user=login;
+		this.password=psw;
+		try {
+			this.connexion(login,psw);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return "erreur de login/password<form method=\"POST\" action=\"auth\"><input name=\"login\" placeholder=\"login\" /><br /><input type=\"password\" name=\"psw\" placeholder=\"password\" /><br /><input type=\"submit\" value=\"Submit\"><br /></form>";
+		}
+		return "vous êtes maintenant connectez à [serveur]";
+	 }
+	
 	@GET
 	@Path("var: .*")
 	@Produces("application/octet-stream")
 	public String getStuff( @PathParam("var") String path ) {
 		InputStream is;
+		
 		try {
 			is = ftp.retrieveFileStream(path);
 			BufferedReader br = new BufferedReader(new InputStreamReader(is));
