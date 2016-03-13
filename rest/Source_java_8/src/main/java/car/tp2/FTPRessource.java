d8834d8515cd5231ae879b391c575e41b50774a9
Bafou <antoine-59630@hotmail.fr>
2016-03-13 20:17:45 +0100

    GET sur dossier fonctionne, GET sur fichier par encore fonctionnel

--- rest/Source_java_8/src/main/java/car/tp2/FTPRessource.java
+++ rest/Source_java_8/src/main/java/car/tp2/FTPRessource.java
@@ -1,9 +1,7 @@
 package car.tp2;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.SocketException;
 
 import javax.ws.rs.FormParam;
@@ -12,8 +10,10 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
 
 import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPFile;
 
 @Path("/ftp")
 public class FTPRessource {
@@ -37,7 +37,7 @@ public class FTPRessource {
 		System.out.println("no erreur");
 		System.out.println(ftp.getReplyString());
 		System.out.println(ftp.pass(password));
-		System.out.println(ftp.getReplyString());
+		System.out.println(ftp.getReplyString());	
 	}
 
 	@GET
@@ -63,19 +63,77 @@ public class FTPRessource {
 	 }
 	
 	@GET
-	@Path("var: .*")
-	@Produces("application/octet-stream")
+	@Path("{var: .*}")
+	@Produces("text/html")
 	public String getStuff( @PathParam("var") String path ) {
 		InputStream is;
-		
+		if (!ftp.isConnected()){
+			try {
+				connexion(user,password);
+			}
+			catch (IOException e) { 
+				e.printStackTrace();
+				return e.toString();
+			}
+		}
 		try {
-			is = ftp.retrieveFileStream(path);
-			BufferedReader br = new BufferedReader(new InputStreamReader(is));
-			return br.toString();
+			String html = "<h1>Server files</h1>\n" ;
+			if (!("").equals(path)) {
+				path += "/";
+			}
+			for(FTPFile f : ftp.listFiles(path)){
+				if(!f.getName().equals(".")){
+					html += "<p>" ;
+					html += "Name : " + f.getName() + " / ";
+					html += "Size in bytes : " + f.getSize() + " / ";
+					if(f.isFile()){
+						html += "  <a href =" + "http://localhost:8080/rest/tp2/ftp/download/" + path  + f.getName()+ ">Download</a>\n" ;
+					}
+					if(f.isDirectory()){
+						html += "  <a href =" + "http://localhost:8080/rest/tp2/ftp/" + path + f.getName()+ ">Open</a>\n" ;
+					}
+					html += "</p>";
+				}
+			}
+			return html;
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 			return e.toString(); 
 		}
 	 }
+	
+	@GET
+	@Produces("application/octet-stream")
+	@Path("download/{filename}")
+	public Response getFile(@PathParam("filename") String filename) {
+		InputStream in;
+		try {
+			in = this.ftp.retrieveFileStream(filename);
+			Response response = Response.ok(in).build();
+			ftp.completePendingCommand();
+			return response;
+		} catch (IOException e) {
+			System.out.print("Erreur lors du téléchargement du fichier :" + filename);
+		}
+       return null;
+	}
+
+	@GET
+	@Produces("application/octet-stream")
+	@Path("download/{var: .*}/{filename}")
+	public Response getFile(@PathParam("var") String pathname, @PathParam("filename") String filename) {
+		InputStream in;
+		try {
+			in = this.ftp.retrieveFileStream(pathname + "/" + filename);
+			Response response = Response.ok(in).build();
+			ftp.completePendingCommand();
+			return response;
+		} catch (IOException e) {
+			System.out.print("Erreur lors du téléchargement du fichier :" + filename);
+		}
+       return null;
+	}
+	
+
 }
