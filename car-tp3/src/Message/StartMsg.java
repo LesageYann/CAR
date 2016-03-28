81e510a1e30aa5115d2dacaad0aa79675226d51a
Valtena <ethanvaltena@gmail.com>
2016-03-28 22:55:52 +0200

    regarde le sms tu saura ce que j'en penses

--- car-tp3/src/Message/StartMsg.java
+++ car-tp3/src/Message/StartMsg.java
@@ -1,19 +1,14 @@
 package Message;
 
-public class StartMsg {
+public class StartMsg extends Msg{
 	private String startNode;
-	private String message;
 
-	public StartMsg(String startNodeName, String message){
+	public StartMsg(int id, String startNodeName, String message){
+		super(id, message);
 		this.startNode= startNodeName;
-		this.message = message;
 	}
 	
 	public Boolean isStartNode(String name){
 		return this.startNode.equals(name);
-	}
-	
-	public String getMsg(){
-		return this.message;
 	}
 }
